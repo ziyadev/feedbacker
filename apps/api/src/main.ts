0c5cd48501a0ab6009c542354b8d48ccc8998c30
ziyaDev <125736719+ziyaDev@users.noreply.github.com>
2025-01-29 23:27:15 +0100

    refactor: remove app module and related files; reorganize module structure for improved clarity

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './modules/app/app.module';
 import { appConfig } from './common/config/app.config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
